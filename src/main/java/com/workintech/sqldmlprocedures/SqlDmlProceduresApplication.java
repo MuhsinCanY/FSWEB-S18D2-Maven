ea98117d5a92e1aa82cbc18351457610cb38610d
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-05-01 20:09:05 +0300

    S18D2 Solution.

--- src/main/java/com/workintech/sqldmlprocedures/SqlDmlProceduresApplication.java
+++ src/main/java/com/workintech/sqldmlprocedures/SqlDmlProceduresApplication.java
@@ -10,4 +10,45 @@ public class SqlDmlProceduresApplication {
 		SpringApplication.run(SqlDmlProceduresApplication.class, args);
 	}
 
+//	8) Öğrenci tablosunu kontrol etmek amaçlı tüm öğrencileri
+//	görüntüleyen "ogrencilistesi" adında bir fonksiyon oluşturun.
+
+//	create or replace function
+//	public.ogrencilistesi()
+//	returns setof ogrenci
+//	language 'sql'
+//	as $BODY$
+//	select * from ogrenci
+//	$BODY$
+
+
+//	9)kitap tablosuna yeni kitap eklemek için "ekle"
+//	adında bir prosedür oluşturun.
+//
+//
+//	create or replace procedure
+//	public.ekle(
+//			in k_ad character varying,
+//			in k_puan integer,
+//			in k_yazarno bigint,
+//			in k_turno bigint
+//	)
+//	language 'sql'
+//	as $BODY$
+//	insert into
+//	public.kitap(ad,puan,yazarno,turno)
+//	values(k_ad,k_puan,k_yazarno,k_turno)
+//	$BODY$
+
+
+//	10) Öğrenci noya göre öğrenci silebilmeyi sağlayan "sil"
+//	adında bir prosedür oluşturun.
+//
+//	create or replace procedure
+//	public.sil(in ogrid bigint)
+//	language 'sql'
+//	as $BODY$
+//	delete from ogrenci
+//	where ogrno = ogrid
+//	$BODY$
 }
